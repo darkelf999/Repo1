106da47b8f995a6d623a5fcbea67db71865bf27a
darkelf999 <thedarkelf980@gmail.com>
2022-02-04 23:15:36 +0530

    changed show

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Hello{
     public static void show(){
         System.out.println("Hi");
+        System.out.println("Hi");
+        int n =10;
     }
-    
+
     public static void show2(){
         System.out.println("Hieee");
     }
